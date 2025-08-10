50b0af144020988cdfbec77fcc233dd0cc468cd9
Alena Provotorova <lunichkina.alena@gmail.com>
2025-08-10 21:47:43 +0300

    test cloudinary2

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,69 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FileEntity, FileType } from './entities/file.entity';
 import { Repository } from 'typeorm';
-import { mkdirSync, renameSync, existsSync, unlinkSync } from 'fs';
+import { v2 as cloudinary } from 'cloudinary';
+import { unlinkSync, existsSync } from 'fs';
 
 @Injectable()
 export class FilesService {
   constructor(
     @InjectRepository(FileEntity)
     private repository: Repository<FileEntity>,
-    ) {}
+  ) {}
 
   findAll(fileType: FileType, folderId: number) {
-    const qb = this.repository.createQueryBuilder('file');  
+    const qb = this.repository.createQueryBuilder('file');
 
-     qb.where('file.folderId = :folderId', { folderId })  
-     if (fileType === FileType.PHOTOS) {
+    qb.where('file.folderId = :folderId', { folderId });
+    if (fileType === FileType.PHOTOS) {
       qb.andWhere('file.mimetype LIKE :type', { type: '%image%' });
-     }
-     if (fileType === FileType.TRASH) {
+    }
+    if (fileType === FileType.TRASH) {
       qb.withDeleted().andWhere('file.deletedAt IS NOT NULL');
-     }
-     return qb.getMany(); 
+    }
+    return qb.getMany();
   }
 
-  create(file: Express.Multer.File, folderId: string, updatedPath: string) {
-    const uploadPath = 'uploads';
-    const folderPath = `${uploadPath}`;
-    if (!existsSync(folderPath)) {
-      mkdirSync(folderPath, { recursive: true });
+  async uploadToCloudinaryAndSave(
+    file: Express.Multer.File,
+    folderId: number,
+  ): Promise<FileEntity> {
+    if (!file) {
+      throw new InternalServerErrorException('Файл не передан');
     }
-    let newPath = '';
-    if(updatedPath.includes(uploadPath)) {
-      newPath = updatedPath;
-    } else {
-      newPath = `${folderPath}/${updatedPath}`;
-    }
-    renameSync(file.path, newPath);
 
-      return this.repository.save({
-      filename: file.filename,
-      originalName: file.originalname,
-      size: file.size,
-      mimetype: file.mimetype,
-      folderId: folderId,
-      path: newPath,
-    });
+    try {
+      const result = await cloudinary.uploader.upload(file.path, {
+        folder: folderId.toString(),
+        resource_type: 'auto',
+      });
+
+      if (existsSync(file.path)) {
+        unlinkSync(file.path);
+      }
+
+      const savedFile = await this.repository.save({
+        filename: file.filename,
+        originalName: file.originalname,
+        size: file.size,
+        mimetype: file.mimetype,
+        folderId: folderId.toString(),
+        path: result.secure_url,
+      });
+
+      return savedFile;
+    } catch (error) {
+      if (file && existsSync(file.path)) {
+        unlinkSync(file.path);
+      }
+
+      console.error('Ошибка загрузки в Cloudinary:', error);
+      throw new InternalServerErrorException('Ошибка загрузки файла');
+    }
   }
 
   async remove(ids: string, folderId: number) {
-    const idsArray = ids.split(',')
+    const idsArray = ids.split(',');
     for (const id of idsArray) {
-      const file = await this.repository.findOneOrFail({ where: { id: Number(id) } });
-      const filePath = `uploads/${file.filename}`;
-      if (existsSync(filePath)) {
-        unlinkSync(filePath);
+      const file = await this.repository.findOneOrFail({
+        where: { id: Number(id) },
+      });
+      if (file.path && file.path.startsWith('uploads/')) {
+        if (existsSync(file.path)) {
+          unlinkSync(file.path);
+        }
       }
     }
-    const qb =  this.repository
-    .createQueryBuilder('file')
-    .where('id IN(:...ids) AND folderId = :folderId', { ids: idsArray, folderId })
-    .softDelete()
-    .execute();
+    const qb = this.repository
+      .createQueryBuilder('file')
+      .where('id IN(:...ids) AND folderId = :folderId', {
+        ids: idsArray,
+        folderId,
+      })
+      .softDelete()
+      .execute();
     return qb;
-  } 
+  }
 
   async updatePath(fileId: number, newPath: string): Promise<FileEntity> {
     await this.repository.update(fileId, { path: newPath });
